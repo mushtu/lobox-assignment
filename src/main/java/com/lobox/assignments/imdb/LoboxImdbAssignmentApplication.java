5e653614241e4a875cae15f132f1c0f30e4ff807
Mojtaba <mojtabayazdani88@gmail.com>
2023-01-13 05:46:24 +0330

    feat: Setup the project

---
+++ src/main/java/com/lobox/assignments/imdb/LoboxImdbAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.lobox.assignments.imdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoboxImdbAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoboxImdbAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lobox/assignments/imdb/LoboxImdbAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lobox.assignments.imdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoboxImdbAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
