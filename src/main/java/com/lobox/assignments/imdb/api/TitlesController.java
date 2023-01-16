db4d747bc44240cf3f1be6ef1abb77fd08681ce6
Mojtaba <mojtabayazdani88@gmail.com>
2023-01-16 04:47:07 +0330

    feat: Add new endpoint

---
+++ src/main/java/com/lobox/assignments/imdb/api/TitlesController.java
@@ -0,0 +1,24 @@
+package com.lobox.assignments.imdb.api;
+
+import an.awesome.pipelinr.Pipeline;
+import com.lobox.assignments.imdb.application.domain.models.PageRequest;
+import com.lobox.assignments.imdb.application.dto.TitleDto;
+import com.lobox.assignments.imdb.application.usecases.GetTitlesWrittenDirectedByTheSameAlivePerson;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/titles")
+public class TitlesController {
+    private final Pipeline pipeline;
+
+    public TitlesController(Pipeline pipeline) {
+        this.pipeline = pipeline;
+    }
+
+    @GetMapping("written-directed-same-alive-person")
+    public Iterable<TitleDto> GetTitlesWrittenDirectedByTheSameAlivePerson(PageRequest<String> pageRequest) {
+        return new GetTitlesWrittenDirectedByTheSameAlivePerson(pageRequest).execute(pipeline);
+    }
+}
